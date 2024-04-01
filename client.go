fbe1c171c6b075b500c90dcf35c2570e9630c697
Martin Gallauner <martin.gallauner@pm.me>
2024-04-01 08:59:06 +0200

    adds mapping of OpenLibrary to Book

--- client.go
+++ client.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -26,26 +29,64 @@ const (
 func (c *Client) FetchBook(isbn string) (Book, error) {
 	url := baseURL + "books?jscmd=data&format=json&bibkeys=ISBN:" + isbn
 
-	req, error := http.NewRequest("GET", url, nil)
-	if error != nil {
-		return Book{}, error
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Book{}, err
 	}
 
-	resp, error := c.httpClient.Do(req)
-	if error != nil {
-		return Book{}, error
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return Book{}, err
 	}
 	defer resp.Body.Close()
 
-	dat, error := io.ReadAll(resp.Body)
-	if error != nil {
-		return Book{}, error
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Book{}, err
 	}
 
-	response := Book{}
-	error = json.Unmarshal(dat, &response)
-	if error != nil {
-		return Book{}, error
+	var response map[string]map[string]interface{}
+	err = json.Unmarshal(dat, &response)
+	if err != nil {
+		return Book{}, err
 	}
-	return response, nil
+
+	book, err := mapBookResponse(response)
+	if err != nil {
+		fmt.Errorf("Failed mapping response to book, %w", err)
+	}
+
+	return book, nil
+}
+
+// mapBookResponse reads the ugly response from the OpenLibraryAPI and maps it to a simple book entity.
+func mapBookResponse(response map[string]map[string]interface{}) (Book, error) {
+	var book Book
+	for isbn, bookData := range response {
+		book.URL = bookData["url"].(string)
+		book.ISBN = strings.TrimPrefix(isbn, "ISBN:")
+		book.Title = bookData["title"].(string)
+
+		pages, err := strconv.Atoi(bookData["pagination"].(string))
+		publish, err := strconv.Atoi(bookData["publish_date"].(string))
+		if err != nil {
+			return book, err
+		}
+		book.Pages = pages
+		book.PublishDate = publish
+
+		authors := bookData["authors"].([]interface{})
+		for _, author := range authors {
+			authorMap := author.(map[string]interface{})
+			authorName := authorMap["name"].(string)
+			book.Authors = append(book.Authors, authorName)
+		}
+		subjects := bookData["subjects"].([]interface{})
+		for _, subject := range subjects {
+			subjectsMap := subject.(map[string]interface{})
+			subjectName := subjectsMap["name"].(string)
+			book.Subjects = append(book.Subjects, subjectName)
+		}
+	}
+	return book, nil
 }
--- type_book.go
+++ type_book.go
@@ -1,29 +1,11 @@
 package main
 
-type Authors struct {
-	URL  string `json:"url"`
-	Name string `json:"name"`
-}
-type Identifiers struct {
-	Isbn13      []string `json:"isbn_13"`
-	Openlibrary []string `json:"openlibrary"`
-}
-type Publishers struct {
-	Name string `json:"name"`
-}
-type Subjects struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
-}
 type Book struct {
-	URL         string       `json:"url"`
-	Key         string       `json:"key"`
-	Title       string       `json:"title"`
-	Authors     []Authors    `json:"authors"`
-	Pagination  string       `json:"pagination"`
-	Weight      string       `json:"weight"`
-	Identifiers Identifiers  `json:"identifiers"`
-	Publishers  []Publishers `json:"publishers"`
-	PublishDate string       `json:"publish_date"`
-	Subjects    []Subjects   `json:"subjects"`
+	ISBN        string
+	URL         string   `json:"url"`
+	Title       string   `json:"title"`
+	Authors     []string `json:"authors"`
+	Pages       int      `json:"pagination"`
+	PublishDate int      `json:"publish_date"`
+	Subjects    []string `json:"subjects"`
 }
