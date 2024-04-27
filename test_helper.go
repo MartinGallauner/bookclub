4065f2743f867e532f4bf17e1e748bf3b0a984dc
Martin Gallauner <martin.gallauner@pm.me>
2024-04-27 07:53:38 +0200

    extracting test helper, deleting obsolete tests

---
+++ test_helper.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"log"
+	"testing"
+	"time"
+)
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
+}
+
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
+}
+
+type PostgresContainer struct {
+	*postgres.PostgresContainer
+	ConnectionString string
+}
+
+// helper method to run for each test //todo please don't start a new container for each test
+func setupTestcontainer() (*BookclubServer, error) {
+	container, err := CreatePostgresContainer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := SetupDatabase(container.ConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := NewBookclubServer(Client{}, &PostgresBookRepository{Database: db}, &PostgresUserRepository{Database: db})
+	return s, err
+}
+
+var (
+	ctx = context.Background()
+)
+
+func CreatePostgresContainer() (*PostgresContainer, error) {
+	postgresContainer, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("docker.io/postgres:15.2-alpine"),
+		postgres.WithDatabase("bookclub-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("password"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	connStr, err := postgresContainer.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	return &PostgresContainer{
+		PostgresContainer: postgresContainer,
+		ConnectionString:  connStr,
+	}, nil
+}
