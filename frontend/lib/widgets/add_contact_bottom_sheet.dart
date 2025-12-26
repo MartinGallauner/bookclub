e2814cd2b498827f64f0f67e0595ef428bcd6d2d
Martin Gallauner <martin@con.io>
2025-12-26 11:12:51 +0100

    improves search page

--- frontend/lib/widgets/add_contact_bottom_sheet.dart
+++ frontend/lib/widgets/add_contact_bottom_sheet.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AddContactBottomSheet extends StatelessWidget {
   const AddContactBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Center(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Add a new contact'),
+          ),
+          SizedBox(height: 20),
+          FractionallySizedBox(
+            widthFactor: 0.8,
+            child: SearchBar(),
+          )
+        ],
+      ),
+
+    );
   }
 
 }
