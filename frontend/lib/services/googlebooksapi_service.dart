e44fe6387ccfa06b5524b3886201f92c70aed646
Martin Gallauner <martin@con.io>
2026-01-06 10:49:00 +0100

    Changing to Google Books API

--- frontend/lib/services/googlebooksapi_service.dart
+++ frontend/lib/services/googlebooksapi_service.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 class GoogleBooksAPIService {
   /// This class provides service methods to call the OpenLibary.org API.
 
-  static const String baseUrl = 'https://openlibrary.org';
+  static const String baseUrl = 'https://www.googleapis.com';
 
   Future<String> searchByISBN(String isbn) async {
     return "1984";
--- frontend/test/services/googlebooksapi_service_test.dart
+++ frontend/test/services/googlebooksapi_service_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/services/googlebooksapi_service.dart';
 
 void main() {
-group('OpenLibraryService', () {
+group('GoogleBooksAPI', () {
   group('searchByISBN', () {
     test('returns book data when valid', () async {
       final service = GoogleBooksAPIService();
