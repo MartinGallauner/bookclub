a78401fd3e3c4d9f2f230ba1b60357bcb5996007
Martin Gallauner <martin@con.io>
2026-01-06 10:13:36 +0100

    adds simple first test

--- frontend/lib/services/openlibrary_service.dart
+++ frontend/lib/services/openlibrary_service.dart
@@ -1,6 +1,12 @@
+import 'dart:math';
+
 class OpenLibraryService {
   /// This class provides service methods to call the OpenLibary.org API.
 
   static const String baseUrl = 'https://openlibrary.org';
+
+  Future<String> searchByISBN(String isbn) async {
+    return "1984";
+  }
 
 }
--- frontend/test/services/openlibrary_service_test.dart
+++ frontend/test/services/openlibrary_service_test.dart
@@ -1,9 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/services/openlibrary_service.dart';
 
 void main() {
 group('OpenLibraryService', () {
   group('searchByISBN', () {
-    test('returns book data when valid', () {});
+    test('returns book data when valid', () async {
+      final service = OpenLibraryService();
+      const isbn = '0451524934';
+      final result = await service.searchByISBN(isbn);
+
+      expect(result, "1984");
+
+
+
+    });
     test('throws when empty', () {});
   });
 
