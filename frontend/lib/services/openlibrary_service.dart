1e3d8cf6eadd78e9c1e2797dd11bee620b666942
Martin Gallauner <martin@con.io>
2026-01-06 10:07:15 +0100

    setup test code

--- frontend/lib/services/openlibrary_service.dart
+++ frontend/lib/services/openlibrary_service.dart
@@ -1,6 +1,5 @@
-/// This class provides service methods to call the OpenLibary.org API.
-
 class OpenLibraryService {
+  /// This class provides service methods to call the OpenLibary.org API.
 
   static const String baseUrl = 'https://openlibrary.org';
 
--- frontend/test/services/openlibrary_service_test.dart
+++ frontend/test/services/openlibrary_service_test.dart
@@ -1,5 +1,16 @@
-class OpenLibraryServiceTest {
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+group('OpenLibraryService', () {
+  group('searchByISBN', () {
+    test('returns book data when valid', () {});
+    test('throws when empty', () {});
+  });
 
-
+  group('searchByTitle', () {
+    test('returns results', () {});
+  });
+});
 }
+
+
