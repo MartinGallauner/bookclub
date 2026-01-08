1024be5f19a5ea4c984b4ba364891c5b1d888afc
Martin Gallauner <martin@con.io>
2026-01-08 18:35:56 +0100

    adds simple book dto

---
+++ frontend/lib/models/book.dart
@@ -0,0 +1,15 @@
+class Book {
+  final String isbn;
+  final String title;
+  final String author;
+
+  const Book({
+    required this.title,
+    required this.author,
+    required this.isbn,
+
+  });
+
+
+}
+
