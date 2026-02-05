a75930ce67e46d0711480f95e8135c28353ce7b2
Martin Gallauner <martin@con.io>
2026-02-05 12:19:50 +0100

    adding a simple simple login page

---
+++ frontend/lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/services/auth_service.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("Please login"),
+          FilledButton.icon(
+              onPressed: AuthService().signInWithGoogle, label: Text("Sign in with Google"),)
+        ],
+      ),
+    );
+  }
+
+
+}
