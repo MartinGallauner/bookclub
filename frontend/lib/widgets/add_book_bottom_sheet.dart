24c9ede64d909b5cb7e3b5a9a0880882f7529386
Martin Gallauner <martin.gallauner@pm.me>
2026-02-05 18:39:46 +0100

    the add book sheet needs to be stateful to actually show the search results

--- frontend/lib/widgets/add_book_bottom_sheet.dart
+++ frontend/lib/widgets/add_book_bottom_sheet.dart
@@ -1,12 +1,17 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:frontend/services/googlebooksapi_service.dart';
 
 /// This bottom sheet offers input fields to add a new book
-
-class AddBookBottomSheet extends StatelessWidget {
+class AddBookBottomSheet extends StatefulWidget {
   const AddBookBottomSheet({super.key});
 
+  @override
+  State<AddBookBottomSheet> createState() => _AddBookBottomSheetState();
+}
+
+class _AddBookBottomSheetState extends State<AddBookBottomSheet> {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -14,24 +19,34 @@ class AddBookBottomSheet extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text("Go ahead and add a book from your physical bookshelf!"),
+            child: Text(
+              "Go ahead and add a book from your physical bookshelf!",
+            ),
           ),
           SizedBox(height: 20),
           FractionallySizedBox(
             widthFactor: 0.8,
-            child: SearchBar(),
+            child: TextField(
+              onSubmitted: (isbn) async {
+                final book = await GoogleBooksAPIService().searchByISBN(isbn);
+                log(book.title);
+              },
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Enter ISBN',
+              ),
+            ),
           ),
           SizedBox(height: 20),
           FilledButton.icon(
-              onPressed: () {
-                log('pressed book scan button');
-              },
-              icon: Icon(Icons.camera_alt),
-              label: Text("Scan ISBN code"))
+            onPressed: () {
+              log('pressed book scan button');
+            },
+            icon: Icon(Icons.camera_alt),
+            label: Text("Scan ISBN code"),
+          ),
         ],
-
       ),
     );
   }
-
 }
