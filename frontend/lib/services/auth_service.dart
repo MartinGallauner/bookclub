289dea5fd26acdebc4665498a86b236c125087ee
Martin Gallauner <martin@con.io>
2026-02-05 10:46:51 +0100

    adds auth service skeleton

---
+++ frontend/lib/services/auth_service.dart
@@ -0,0 +1,9 @@
+class AuthService {
+  /// This class provides service functions for everything concerned with (firebase) authentication
+
+  void signInWithGoogle() {}
+
+  void signOut() {}
+
+  void authStateChanges() {}
+}
