48832a35eed2af998ec56609c5c1a677b08b7add
Martin Gallauner <martin.gallauner@pm.me>
2026-02-06 09:26:29 +0100

    starting to build library_service class

---
+++ frontend/lib/services/library_service.dart
@@ -0,0 +1,10 @@
+import '../models/book.dart';
+
+class LibraryService {
+
+  late List<Book> collection;
+
+  LibraryService() {
+    collection = List.empty();
+  }
+}
---
+++ frontend/test/services/library_service_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/services/library_service.dart';
+
+void main () {
+  group('LibraryService', () {
+    group('New Library', () {
+      test('New library is empty', () {
+        final service = LibraryService();
+
+    });
+    });
+
+  });
+
+}
