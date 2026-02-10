e72edb4e691993bac01a54709774f9e1d906d5c2
Martin Gallauner <martin.gallauner@pm.me>
2026-02-10 10:57:59 +0100

    adding test boilerplate for AuthServcie

---
+++ frontend/test/services/auth_service_test.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/services/auth_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+class MockUserCredential extends Mock implements UserCredential {}
+class MockUser extends Mock implements User {}
+class FakeAuthProvider extends Fake implements AuthProvider {}
+
+void main() {
+  setUpAll(() {
+    registerFallbackValue(FakeAuthProvider());
+  });
+
+  group('Auth Service', () {
+    test('if new signup, create new profile', () async {
+      final mockAuth = MockFirebaseAuth();
+      final authService = AuthService(mockAuth);
+
+      final mockUser = MockUser();
+      final mockUserCredential = MockUserCredential();
+
+      when(() => mockUser.uid).thenReturn('test-uid-123');
+      when(() => mockUser.email).thenReturn('test@example.com');
+      when(() => mockUser.displayName).thenReturn('Test User');
+      when(() => mockUser.photoURL).thenReturn('https://example.com/photo.jpg');
+      when(() => mockUserCredential.user).thenReturn(mockUser);
+
+      when(() => mockAuth.signInWithPopup(any()))
+          .thenAnswer((_) async => mockUserCredential);
+
+      User? user =  (await authService.signInWithGoogle()).user;
+
+
+    });
+
+  });
+}
