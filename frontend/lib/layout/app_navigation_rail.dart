f7c001e970eae6b30dc7393999ef37d211e2021e
Martin Gallauner <martin.gallauner@pm.me>
2026-02-11 14:51:10 +0100

    fixing typo

--- frontend/lib/layout/app_navigation_rail.dart
+++ frontend/lib/layout/app_navigation_rail.dart
@@ -28,7 +28,7 @@ class AppNavigationRail extends StatelessWidget {
           ),
           NavigationRailDestination(
             icon: Icon(Icons.person),
-            label: Text('Conntacts'),
+            label: Text('Contacts'),
           ),
         ],
         selectedIndex: selectedIndex,
