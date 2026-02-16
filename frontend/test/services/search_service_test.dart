f5fd1d0ad92d5b1400dc398a3e59ddad5bb16375
Martin Gallauner <martin.gallauner@pm.me>
2026-02-16 08:49:02 +0100

    first step of search_searvice

---
+++ frontend/test/services/search_service_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/services/search_service.dart';
+
+void main() {
+  group('Search Services Tests', () {
+    test('search in network', () {
+      final service = SearchService();
+
+});
+
+});
+
+}
